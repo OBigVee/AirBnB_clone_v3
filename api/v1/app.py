7e8253002cef6728bdbcf63188d66656d7ce7fe1
OBigVee <adedejivictor98@gmail.com>
2023-09-13 09:20:49 +0100

    status of API checked:heavy_check_mark:

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script returns the status of api"""
+
+import os
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def close_storage(exception=None):
+    """close current SQLalchmey session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """returns 404 err"""
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    HBNB_API_HOST = os.environ.get("HBNB_API_HOST") or "0.0.0.0"
+    HBNB_API_PORT = os.environ.get("HBNB_API_PORT") or 5000
+    app.run(HBNB_API_HOST, HBNB_API_PORT, threaded=True)
